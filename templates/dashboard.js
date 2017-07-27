195d0fd34a6550775c387548bb01dff968308af4
khanhthong <thonglk.mac@gmail.com>
2017-07-27 14:04:58 +0700

    add tab sai gon ha noi

--- templates/dashboard.js
+++ templates/dashboard.js
@@ -5,6 +5,7 @@ angular.module('app').controller('dashboardCtrl', dashboardCtrl);
 function dashboardCtrl($scope, $timeout, $sce, toastr, $state, CONFIG, $http, $rootScope) {
     $scope.loading = true
     $rootScope.aside = false
+    $scope.showjob =false;
 
 
     if (!$rootScope.UserCard && !$rootScope.StoreCard) {
