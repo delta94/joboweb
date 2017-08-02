64f74316d88762218b775f7888a3b40d71350887
System Administrator <root@Les-MacBook-Pro.local>
2017-08-02 15:43:14 +0700

    phone show

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ angular.module('starter.configs', [])
 
     .constant("CONFIG", {
         'UpdateAt': "2017-04-10T04:44:21.253Z",
-        'Location': false,
+        'Location': true,
         'WEBURL': 'https://joboapp.com',
-        "APIURL": 'http://localhost:8080',
+        "APIURL": 'https://jobohihi.herokuapp.com',
         'FCM_KEY': "AAAArk3qIB4:APA91bEWFyuKiFqLt4UIrjUxLbduQCWJB4ACptTtgAovz4CKrMdonsS3jt06cfD9gGOQr3qtymBmKrsHSzGhqyJ_UWrrEbA4YheznlqYjsCBp_12bNPFSBepqg_qrxwdYxX_IcT9ne5z6s02I2mu2boy3VTN3lGPYg",
         "APIKey": 'AIzaSyATOX9rL_ULV-Q_e2kImu9wYgK2AToOteQ',
         "StatusChat": {
